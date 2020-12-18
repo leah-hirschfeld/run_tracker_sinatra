f58778781d59f2578844bfe3f33ff9e188104b87
leah_hirschfeld <leah.hirschfeld@gmail.com>
2020-12-17 19:54:27 -0800

    updated index to list runs

--- run_tracker_sinatra/app/controllers/runs_controller.rb
+++ run_tracker_sinatra/app/controllers/runs_controller.rb
@@ -1,6 +1,7 @@
 class RunsController < ApplicationController
 
     get "/runs" do #index
+        @runs = Run.all
         erb :"runs/index"
     end
 
--- run_tracker_sinatra/db/seeds.rb
+++ run_tracker_sinatra/db/seeds.rb
@@ -1,1 +1,3 @@
-Run.create(length: "5k", date: 12-14-2020, location: "Oakland", description: "fast easy run")
+Run.create(length: "5k", date: "12-14-2020", location: "Oakland", description: "fast easy run")
+Run.create(length: "10K", date: "12-20-2020", location: "Chicago", description: "hard, longer run")
+Run.create(length: "Sprints", date: "12-18-2020", location: "Oakland", description: "Quarter mile repeats")
