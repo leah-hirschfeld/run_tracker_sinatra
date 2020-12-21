bfe902ea3f32a81f1e080f87233a76e37cae26ac
leah_hirschfeld <leah.hirschfeld@gmail.com>
2020-12-21 15:16:05 -0800

    updated current user method to single line code

--- run_tracker_sinatra/app/controllers/application_controller.rb
+++ run_tracker_sinatra/app/controllers/application_controller.rb
@@ -19,12 +19,8 @@ class ApplicationController < Sinatra::Base
     end
 
     def current_user
-      if @current_user
-        @current_user
-      else
-        @current_user = User.find_by(id: session[:user_id])
-      end
+      @current_user ||= User.find_by(id: session[:user_id])
     end
-  end
+    end
 
 end
