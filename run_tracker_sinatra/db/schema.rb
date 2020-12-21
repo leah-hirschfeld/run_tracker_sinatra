a2b3b71520a05b1fc522c2fd6a64cd4fc191ac9b
leah_hirschfeld <leah.hirschfeld@gmail.com>
2020-12-20 19:08:01 -0800

    fixed mispelling of Users table

--- run_tracker_sinatra/db/schema.rb
+++ run_tracker_sinatra/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201217231626) do
+ActiveRecord::Schema.define(version: 20201221030610) do
 
   create_table "runs", force: :cascade do |t|
     t.string "length"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 20201217231626) do
   end
 
   create_table "useres", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+  end
+
+  create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
   end
